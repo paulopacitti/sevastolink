03284edf4c47c944f81f008d34e76dfaf240c0be
Paulo Pacitti <ppacitti@outlook.com>
2020-08-18 17:50:10 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# sevastolink_ :alien:
+
+- A color theme based on **Sevastolink**, [Sevastopol's](https://alienanthology.fandom.com/wiki/Sevastopol_Station) desktop OS presented in SEGA's "[Alien: Isolation](https://en.wikipedia.org/wiki/Alien:_Isolation)";
+- **This color theme is currently under development!**
+
+![](https://i.pinimg.com/originals/c1/20/9c/c1209cd7b1c0a30307d7abdb4d58b8df.jpg)
+
+## Supports :floppy_disk:
+- iTerm;
+- VS Code;
+
+## Color Palette :art:
+
+Hex       | RGBA  | HSL |   
+---       | ---   | --- |
+`#0c290c` | `rgb(12,41,12,)`   | `120°, 57%, 10%`
+`#134213` | `rgb(19,66,19)`    | `120°, 55%, 17%`
+`#05b669` | `rgb(5, 182, 105)` | `154°, 95%, 37%`
+`#f07826` | `rgb(240, 120, 38)` | `24°, 87%, 55%`
+`#ccd5d4` | `rgb(204, 213, 212)` | `173°, 10%, 82%`
+`#7a807f` | `rgb(122, 128, 127)` | `170°, 2%, 49%`
---
+++ colors.js
@@ -0,0 +1,24 @@
+// This file can be used with the themer CLI, see https://github.com/mjswensen/themer
+
+module.exports.colors = {
+  "dark": {
+    "shade0": "#0C290C",
+    "shade1": "#05B669",
+    "shade2": "#7A807F",
+    "shade3": "#7A807F",
+    "shade4": "#7A807F",
+    "shade5": "#CCD5D4",
+    "shade6": "#7A807F",
+    "shade7": "#CCD5D4",
+    "accent0": "#F07826",
+    "accent1": "#F07826",
+    "accent2": "#F07826",
+    "accent3": "#05B669",
+    "accent4": "#F07826",
+    "accent5": "#F07826",
+    "accent6": "#F07826",
+    "accent7": "#05B669"
+  }
+};
+
+// Your theme's URL: https://themer.dev/?activeColorSet=dark&colors.dark.shade0=%230C290C&colors.dark.shade7=%23CCD5D4&colors.dark.accent0=%23F07826&colors.dark.accent1=%23F07826&colors.dark.accent2=%23F07826&colors.dark.accent3=%2305B669&colors.dark.accent4=%23F07826&colors.dark.accent5=%23F07826&colors.dark.accent6=%23F07826&colors.dark.accent7=%2305B669&colors.dark.shade6=%237A807F&colors.dark.shade3=%237A807F&colors.dark.shade1=%2305B669&colors.dark.shade5=%2305B66&colors.dark.shade4=%237A807F&colors.dark.shade2=%237A807F&calculateIntermediaryShades.dark=false
